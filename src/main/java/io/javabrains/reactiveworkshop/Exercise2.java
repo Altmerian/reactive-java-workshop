eb4cffe66c6a4c2156eb1a99dadeceb7833709a0
Pavel Shakhlovich <Altmersc@gmail.com>
2023-02-04 23:03:03 +0400

    Exercise 2,3

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -2,6 +2,9 @@ package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
 
+import static io.javabrains.reactiveworkshop.ReactiveSources.intNumbersFlux;
+import static io.javabrains.reactiveworkshop.ReactiveSources.userFlux;
+
 public class Exercise2 {
 
     public static void main(String[] args) throws IOException {
@@ -9,13 +12,12 @@ public class Exercise2 {
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
+        intNumbersFlux().subscribe(System.out::println);
 
         // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
+        userFlux().subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
     }
-
 }
--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,6 +1,9 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
+
+import static io.javabrains.reactiveworkshop.ReactiveSources.intNumbersFlux;
 
 public class Exercise3 {
 
@@ -10,11 +13,13 @@ public class Exercise3 {
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
-        // TODO: Write code here
 
+        List<Integer> integers = intNumbersFlux().toStream()
+                .toList();
+        System.out.println(integers);
+        System.out.println(integers.size());
 
-        System.out.println("Press a key to end");
-        System.in.read();
+//        System.out.println("Press a key to end");
+//        System.in.read();
     }
-
 }
